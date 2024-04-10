473b0b47aea1a8b1dc890233b82cd5ce5dd883f3
FESQO <98529236+FESQO@users.noreply.github.com>
2024-04-10 11:19:39 +1000

    dhdhdg

--- post/MainMenu.xaml.cs
+++ post/MainMenu.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using OpenPop.Mime;
 using OpenPop.Pop3;
+using System.Windows.Threading;
 
 namespace post
 {
@@ -24,10 +27,18 @@ namespace post
     /// </summary>
     public partial class MainMenu : Window, INotifyPropertyChanged
     {
+        Pop3Client pop3Client;
         public MainMenu()
         {
             InitializeComponent();
+            pop3Client = ConnectMail();
+            DataContext = this;
         }
+
+        //timer = new DispatcherTimer();
+        //timer.Tick += new EventHandler(timer_Tick);
+        //timer.Interval = new TimeSpan(0,0,1);
+        //timer.Start();
 
         private void Button_Send_Window(object sender, EventArgs e)
         {
@@ -37,15 +48,11 @@ namespace post
 
         private void Button_Upgrate(object sender, RoutedEventArgs e)
         {
-            Pop3Client pop3Client = new Pop3Client();
-
-            var username = "alina1125@suz-ppk.ru";
-            var password = "D35de%TJ";
-
-            pop3Client.Connect("pop3.beget.com", 110, false);
-            pop3Client.Authenticate(username, password, AuthenticationMethod.UsernameAndPassword);
+            if (pop3Client.Connected)
+                pop3Client.Disconnect();
+            pop3Client = ConnectMail();
             int count = pop3Client.GetMessageCount();
-            var emails = new List<POPEmail>();
+            var emails = new ObservableCollection<POPEmail>();
 
             int counter = 0;
             for (int i = count; i >= 1; i--)
@@ -88,29 +95,46 @@ namespace post
                 counter++;
             }
 
-            test.ItemsSource = emails;
+            Email = emails;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Email)));
         }
 
-        private POPEmail POPEmail;
-        public event PropertyChangedEventHandler? PropertyChanged;
-        public List<POPEmail> email { get; set; }
-        public POPEmail pOPEmail
+       
+
+
+        private static Pop3Client ConnectMail()
         {
-            get => POPEmail;
+            Pop3Client pop3Client = new Pop3Client();
+
+            var username = "alina1125@suz-ppk.ru";
+            var password = "D35de%TJ";
+
+            pop3Client.Connect("pop3.beget.com", 110, false);
+            pop3Client.Authenticate(username, password, AuthenticationMethod.UsernameAndPassword);
+            return pop3Client;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ObservableCollection<POPEmail> Email { get; set; }
+        
+        private POPEmail email;
+        public POPEmail POPEmail
+        {
+            get => email;
             set
             {
-                POPEmail = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(pOPEmail)));
+                email = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(POPEmail)));
             }
         }
         
         private void Button_Delete(object sender, RoutedEventArgs e)
         {
-            if (pOPEmail != null)
+            if (POPEmail != null)
                 MessageBox.Show("Выбран обьект");
-            email.Remove(pOPEmail);
-            PropertyChanged?.Invoke(this,
-                    new PropertyChangedEventArgs(nameof(pOPEmail)));
+            pop3Client.DeleteMessage(POPEmail.MessageNumber);
+            Email.Remove(POPEmail);
+
 
 
         }
