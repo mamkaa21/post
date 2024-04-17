e8fc68aa07bd1140366f55485b9357df5aa672dc
FESQO <98529236+FESQO@users.noreply.github.com>
2024-04-17 11:14:10 +1000

    hfgbhdfg

--- post/MySqlDB.cs
+++ post/MySqlDB.cs
@@ -3,28 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using MySqlConnector;
-
-
-
+using System.Windows;
+using MySqlConnector;
 namespace post
 {
     public class MySqlDB
     {
-        //MySqlConnection mySqlConnection;
-        
+        MySqlConnection mySqlConnection;
+        private MySqlDB()
+        {
+            MySqlConnectionStringBuilder stringBuilder = new();
+            stringBuilder.UserID = "student";
+            stringBuilder.Password = "student";
+            stringBuilder.Database = "PostAlina1125";
+            stringBuilder.Server = "192.168.200.13";
+            stringBuilder.CharacterSet = "utf8mb4";
+            mySqlConnection = new MySqlConnection(stringBuilder.ToString());
+            OpenConnection();
+        }
 
-        //private MySqlDB()
-        //{
-        //    MySqlConnectionStringBuilder stringBuilder = new();
-        //    stringBuilder.UserID = "student";
-        //    stringBuilder.Password = "student";
-        //    stringBuilder.Database = "PostAlina1125";
-        //    stringBuilder.Server = "192.168.200.13";
-        //    stringBuilder.CharacterSet = "utf8mb4";
-        //}
+        private bool OpenConnection()
+        {
+            try
+            {
+                mySqlConnection.Open();
+                return true;
+            }
+            catch(Exception ex)  
+            {
+                MessageBox.Show(ex.Message); 
+                return false;
+            }
+        }
 
+        public void CloseConnection()
+        {
+            try
+            {
+                mySqlConnection.Close();                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        internal MySqlConnection GetConnection() 
+        {
+            if (mySqlConnection.State != System.Data.ConnectionState.Open)
+                if (!OpenConnection())
+                    return null;
+
+            return mySqlConnection;
+        }
+
+        static MySqlDB instance;
+        public static MySqlDB Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new MySqlDB();
+                return instance;
+            }
+        }
+
+        public int GetAutoID(string T)
+        {
+            try
+            {
+                string sql = "SHOW POST STATUS WHERE 'Name' = " + T + "'";
+                using (var mc = new MySqlCommand(sql, mySqlConnection))
+                using (var reader = mc.ExecuteReader())
+                {
+                    if (reader.Read())
+                        return reader.GetInt32("Auto_increment");
+                }
+                return -1;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                    return -1;
+            }
+        }
     }
-
-
 }
---
+++ post/PostRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace post
+{
+    public class PostRepository
+    {
+        private PostRepository() { }
+
+        static PostRepository instance;
+        public static PostRepository Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new PostRepository();
+                return instance;
+            }
+        }
+
+        internal IEnumerable<POPEmail> GetAllPOPEmails(string sql)
+        {
+            var result = new ObservableCollection<POPEmail>();
+            var connect = MySqlDB.Instance.GetConnection();
+            if (connect == null) 
+            return result;
+            using (var)
+        }
+    }
+
+   
+}
